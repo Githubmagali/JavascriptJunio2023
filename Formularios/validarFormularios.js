51fefbdc051b0825694547867795c9f1196b2034
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-06-09 18:40:18 -0300

    cambios

--- Formularios/validarFormularios.js
+++ Formularios/validarFormularios.js
@@ -5,12 +5,34 @@ formulario.addEventListener('submit', (e)=>{
     e.preventDefault();
     
     const datos={
-        correo: formulario.correoName.value,
+        correoPro: formulario.correoName.value,
         pais: formulario.paisName.value,
         donacion: formulario.donacionMonto.value,
         fecha:formulario.fecha.value,
-        terminos:formulario['terminos-y-condiciones'].cheked,
+        terminos:formulario['terminos-y-condiciones'].checked,
      
     };
-    console.log(datos);
+    if(datos.correoPro.length <=12){
+        console.log('Correo invalido');
+        return;
+    };
+
+    if(datos.pais=== ''){
+        console.log('pais invalido');
+        return;
+    }
+    if(datos.donacion=== ''){
+        console.log('Selecciona una cantidad');
+        return;
+  }
+  if(datos.donacion=== ''){
+    console.log('Selecciona una cantidad');
+    return;
+}
+
+   if(!datos.terminos){
+    console.log('Acepta los terminos y condiciones');
+    return;
+   }
+      console.log('Enviando datos');
 });
